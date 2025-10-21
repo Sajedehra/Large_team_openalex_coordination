ede62049d271b4207ca7e01c4a5c8c982029fa86
participant <participant@TUE020492.campus.tue.nl>
2025-10-21 11:07:40 +0200

    cleaning after qualitative

--- cleaning.R
+++ cleaning.R
@@ -11,7 +11,7 @@ library(openalexR)
 library(tictoc)
 library(psych)
 library(ggplot2)
-library(stringr)
+
 # initial merging ------------------------------------------------------------
 
 
@@ -92,6 +92,8 @@ dat <- dat |>
 #check2<- sample_n(dat_d, 200, replace = FALSE)
 #check3<- sample_n(dat, 200, replace = FALSE)
 #check4<- sample_n(dat, 200, replace = FALSE)
+#keywordcheck<- rbind(check, check2, check3, check4)
+saveRDS(keywordcheck, "keywordcheck.rds")
 
 irrelevant_journal_keywords<- c("History", "software", "Eos", "Acoustics", "Surgery", "Tourism", "chemistry", "Robotics", "Physical",
                                 "Entrepreneurial Business", "ACM SIGCSE Bulletin", "computer science", "Environmental", "Pharmacology",
@@ -205,7 +207,10 @@ n_info <- data.frame(initial = n_notduplicate,
                      Corr_range = n_correctyear,
                      article = n_article,
                      noeng = n_english,
-                     relevant = n_irrelevant)
+                     relevant = n_irrelevant,
+                     miss_author = n_miss_author,
+                     miss_issn = n_miss_issn,
+                     missing_ref= n_missing_ref)
 
 
 #saving the clean data
@@ -214,29 +219,90 @@ saveRDS(dat, file="openalexdata_cleaned.rds")
 saveRDS(n_info, file= "n_info.rds")
 
 
-# taking the subset of the data and save it
+# cleanings after manual coding ---------------------------------------------------------
+n_dat<- nrow(dat)
+new_keyword<- c("correction", "Book Notes", "Day-by Day", "Respiratory", "Cover-Chewing", "daytime napping", 
+                "immune protection", "CSCL System", "Poster Session", "colonial legacy", "Cardiopulmonary",
+                "blood disorders", "DMD patients", "Upper Limb", "About the Contributors", "acousto-mechanical", "grid computing",
+                "ventilation", "cyberinfrastructure", "PRO-TECT", "geospatial", "interprofessional medication", "Phenotypic",
+                "artery bypass", "Madagascar", "metabolomics", "aesthetic surg", "endometrial", "tertiary care", "deflagration", 
+                "Temparature", "Temperature", "nano-material", "Diffusion", "Infection", "transportation", "tuberculosis",
+                "Photographic", "Vitreoretinopathy", "hybrid mixtures", "Prophylactic", "Abstracts", "lightcurves", "ape decline",
+                "fibrosis", "AQUA", "primate extinction", "particle", "ISOCAM", "diabetes", "Stopwatch", "genetics gap",
+                "Blood Collection", "muscular dystrophy", "Symbolic universes", "Decarboxylase", "ERA Registry", "molecular",
+                "Flow System", "antioxidant", "autopsy", "Natural gas", "OpenModelica", "geological", "Nanotube", "ape extinction",
+                "Arthritis", "Cardiovascular", "genomic epidemiology", "Stem Cell", "atrophy", "debris disk", "bioinformatics",
+                "Antibodies", "aortic", "Telepresence", "narcolepsy", "Traumatic Brain Injury", "Hypoferremia", "Stroke", "oncology",
+                "Galaxy", "Metabolit", "harmonization", "versatility", "ivory", "Ecology", "motor disorder", "Cardiac pacing", "Edible Insects",
+                "entomology", "gas gauge", "endangered species", "hepatitis", "Sydenham Chorea", "Pollinator", "dialysis", "Biomarkers"
+                )
 
-#samdat <- slice_sample(.data = dat, n = 1000000, weight_by = )
+case_sensitive<- c( "HIV","COPD", "serum", "BMI", "IPCC")
 
-#samdat<- sample_n(dat, 1000000)
-#saveRDS(samdat, file="data/samdatc.rds")
+journal<- c("pac", "oncology", "APS")
 
-dat<- readRDS("D:/Sajedeh/Psydat_cleaned.rds")
+ids<- "https://openalex.org/W4382936650
+https://openalex.org/W4200564330
+https://openalex.org/W4286762924
+https://openalex.org/W3097713271
+https://openalex.org/W1992360116
+https://openalex.org/W4312679429
+https://openalex.org/W3215287579
+https://openalex.org/W3043317701
+https://openalex.org/W1967120130
+https://openalex.org/W3203418161
+https://openalex.org/W4282915658
+https://openalex.org/W4391495193
+https://openalex.org/W2542228716
+https://openalex.org/W3196418672
+https://openalex.org/W2026532911
+https://openalex.org/W4400112743
+https://openalex.org/W1514933037
+https://openalex.org/W2055798093
+https://openalex.org/W2285173204
+https://openalex.org/W4220867049
+https://openalex.org/W3159009895
+https://openalex.org/W2325257031
+https://openalex.org/W2795224003"
 
-try<- oa_fetch(identifier = "W4234470470",
-         entity = "works",
-         options = tibble(select = c("display_name"))
-         )
+ids_vec <- strsplit(ids, "\n")[[1]]
 
-dat<- dat|> mutate(oa_identifier = sub(".*/", "", oa_id))
-tic()
-for (i in 1:nrow(dat)) {
-  res<- oa_fetch(identifier = dat$oa_identifier[i],
-                          entity = "works",
-                          options = tibble(select = c("display_name")))
-  if(!is.null(res) && nrow(res) > 0){
-    dat$title[i]<- res$display_name
-  }
-  else{ dat$title[i]<- NA_character_ }
+escape_regex <- function(x) str_replace_all(x, "([\\^$.|?*+()\\[\\]{}])", "\\\\\\1")
+space_to_class <- function(x) str_replace_all(x, "\\p{Zs}+", "\\\\s+")  # any Unicode space
+
+make_pattern <- function(keys) {
+  keys |>
+    escape_regex() |>
+    space_to_class() |>
+    (\(v) paste0("\\b", v, "\\b"))() |>
+    paste(collapse = "|")
 }
-toc()
+
+
+
+n_pattern_title <- make_pattern(new_keyword)
+dat <- dat |>
+  filter(!str_detect(title, regex(n_pattern_title, ignore_case = TRUE))) #28115 removed
+
+
+#reruuning previous one
+pattern2 <- make_pattern(irrelevant_journal_keywords)
+
+dat <- dat |>
+  filter(!str_detect(journal, regex(pattern2, ignore_case = TRUE))) # 1 additional removed
+
+
+n_pattern_case <- paste(case_sensitive, collapse = "|")
+dat <- dat |>
+  filter(!str_detect(title, regex(n_pattern_case, ignore_case = FALSE))) #4266
+
+
+n_pattern_journal <- paste(journal, collapse = "|")
+dat <- dat |>
+  filter(!str_detect(journal, regex(n_pattern_journal, ignore_case = TRUE))) #7788
+
+
+dat <- dat |>
+  filter(!oa_id %in% ids_vec) #22
+
+saveRDS(dat, "final_clean_dat.rds")
